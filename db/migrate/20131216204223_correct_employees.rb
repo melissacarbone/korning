6149dac2ca3d2b558c3f13ec2864a41bd9f4f3f0
Melissa Carbone <mcarbone1@gmail.com>
2013-12-16 15:50:52 -0500

    added and corrected employee migrations

--- db/migrate/20131216204223_correct_employees.rb
+++ db/migrate/20131216204223_correct_employees.rb
@@ -1,11 +1,10 @@
-class SeparateNameInfoInEmployees < ActiveRecord::Migration
+class CorrectEmployees < ActiveRecord::Migration
   def change
     Employee.all.each do |employee|
       info = employee.first_name.split
       employee.first_name = info[0]
       employee.last_name = info[1]
       employee.email = info[2]
+      employee.save!
     end
-  end
 
-end
